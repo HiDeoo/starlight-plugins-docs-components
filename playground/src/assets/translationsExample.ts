daa56674a6a052bc2c9b093fb69748fe9e2e7d3c
trueberryless <99918022+trueberryless@users.noreply.github.com>
2025-02-17 19:52:29 +0100

    i like translations :)

--- playground/src/assets/translationsExample.ts
+++ playground/src/assets/translationsExample.ts
@@ -7,4 +7,8 @@ export const Translations = {
     'example.key1': 'valeur de la clé 1',
     'example.key2': 'valeur de la clé 2',
   },
+  de: {
+    'example.key1': 'Schlüssel 1 Wert',
+    'example.key2': 'Schlüssel 2 Wert',
+  },
 }
